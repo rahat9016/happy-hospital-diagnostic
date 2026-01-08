3d64fb7deb8b9090fa45519ccb8caaf5238bb98b
Minhazur Rahman <75428759+rahat9016@users.noreply.github.com>
2026-01-08 11:41:23 +0600

    Dev/layout (#5)
    * Fix: Button
    
    * Add: DashboardHeader
    
    * fix: build error
    
    * Fix: Removed --prod flag
    
    * trigger PR preview workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import DashboardHeader from "../components/layout/dashboardLayout/DashboardHeader";
 import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
     <div className="container">
-      <Button>Click </Button>
+      <DashboardHeader />
+      <Button>Click here</Button>
     </div>
   );
 }
